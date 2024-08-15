310887eae17cf7f8a6f392e2f1cf8d31b41f61bd
AlettaAlexxanderClaudia <aletta.claudia26@gmail.com>
2024-08-15 10:55:24 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+const queue = [];
+const HAIRCUT_DURATION = 20; // dalam menit
+const BARBER_PHONE_NUMBER = '6289652264369'; // Nomor WhatsApp tujuan pesan dalam format internasional
+
+function updateQueueInfo() {
+    const queueTableBody = document.querySelector('#queue-table tbody');
+    queueTableBody.innerHTML = '';
+
+    queue.forEach((customer, index) => {
+        const waitTime = index * HAIRCUT_DURATION;
+        const row = `<tr>
+                        <td>${index + 1}</td>
+                        <td>${customer.name}</td>
+                        <td>${waitTime} menit</td>
+                    </tr>`;
+        queueTableBody.innerHTML += row;
+    });
+}
+
+function notifyBarber(customer, queueNumber) {
+    const message = `Hallo, Saya\nNama pelanggan  : ${customer.name}\nNo.Antrian  : ${queueNumber}\nNo.Wa : ${customer.phone}`;
+    const encodedMessage = encodeURIComponent(message);
+    const url = `https://wa.me/${BARBER_PHONE_NUMBER}?text=${encodedMessage}`;
+    
+    // Arahkan ke WhatsApp dengan pesan pra-isi
+    window.open(url, '_blank');
+}
+
+document.getElementById('booking-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    const name = document.getElementById('name').value;
+    const phone = document.getElementById('phone').value;
+    
+    const newCustomer = { name, phone };
+    const queueNumber = queue.length + 1;
+
+    // Menampilkan pesan WhatsApp kepada pengguna
+    notifyBarber(newCustomer, queueNumber);
+
+    // Menambahkan customer ke queue dan memperbarui daftar
+    queue.push(newCustomer);
+    updateQueueInfo();
+    this.reset();
+});
+
+document.getElementById('toggle-queue').addEventListener('click', function() {
+    const queueTableSection = document.getElementById('queue-table-section');
+    queueTableSection.style.display = queueTableSection.style.display === 'none' ? 'block' : 'none';
+    this.textContent = queueTableSection.style.display === 'none' ? 'Tampilkan Antrian' : 'Sembunyikan Antrian';
+});
+
+document.getElementById('barber-phone').textContent = `+${BARBER_PHONE_NUMBER}`;
